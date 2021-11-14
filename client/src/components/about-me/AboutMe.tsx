8bb2f412276f037e4f203b5494fba6e970cea590
pgm-jandeschacht <jandesch@student.arteveldehs.be>
2021-11-14 19:08:08 +0100

    Added basic json

---
+++ client/src/components/about-me/AboutMe.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const AboutMe = (props: Props) => {
+    return (
+        <div>
+            
+
+            <ul>
+                Test
+            </ul>
+        </div>
+
+    )
+}
+
+export default AboutMe
--- client/src/pages/about-me.tsx
+++ client/src/pages/about-me.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { AboutMe } from "../components/about-me";
 import Ipage from '../interfaces/page';
 import { BaseLayout } from "../layouts";
 
+// example object agenda
+const user = {
+    id: 1,
+    firstName: "Anna",
+    lastName: "Boloise",
+    gender: "F",
+    dob: "4th august 1949",
+    maritalStatus: "Widow",
+    partner: "John mcCafé",
+    nursingHome: "Maynard Nursing Home",
+    carreer: "Risk Analist at IBM",
+    activeLocation: "Toronto, Canada",
+    image: ""
+}
+
+
 const AboutMePage: React.FunctionComponent<Ipage> = props => {
+
     return (
-        <BaseLayout backgroundStyle={"accent4"} PageTitle={"About me"}>
-           <p>This is the ABOUT ME page!</p>
+        <BaseLayout backgroundStyle={"accent4"} PageTitle={`About ${user.firstName}`} >
+           <AboutMe/>
         </BaseLayout>
     )
 }
