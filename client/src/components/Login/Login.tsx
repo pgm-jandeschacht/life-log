9660216b6f83885eb4930bac90bfd1a4a00383b4
pgm-jandeschacht <jandesch@student.arteveldehs.be>
2021-11-15 12:37:02 +0100

    Added styling to login page

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect} from 'react'
-import './Login.css'
+import styled from 'styled-components'
+import { Colors, Shadow, Transition } from '../../variables'
+import logo from '../../assets/images/logo.png'
 
 async function loginUser(credentials: any) {
     return fetch('http://localhost:3000/login', {
@@ -42,6 +44,106 @@ interface LoginProps {
     setFamilyMemberId: any
 }
 
+const StyledDiv = styled.div`
+    position: fixed;
+    z-index: 1;
+    background: ${Colors.primary};
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+
+    h2 {
+        font-size: 4rem;
+        font-weight: 900;
+        margin-bottom: 5rem;
+    }
+`
+
+
+const StyledLogo = styled.div`
+    background: ${Colors.primary};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 6rem 4rem;
+    
+    img {
+        width: 100%;
+        max-width: 25rem;
+    }
+`
+
+const StyledContainer = styled.div`
+    background: ${Colors.secondary};
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    border-radius: 20px 20px 0 0 ;
+    padding: 4rem 2rem;
+`
+
+const StyledForm = styled.form`
+    
+    label {
+        display: flex;
+        flex-direction: column;
+        font-size: 2.5rem;
+        font-weight: 900;
+        margin-bottom: 2rem;
+
+        input {
+            margin-top: 2rem;
+            border: 3px solid ${Colors.primary};
+            border-radius: 10px;
+            box-shadow: ${Shadow.small};
+            background: ${Colors.secondary};
+            padding: 1.5rem;
+            font-size: 2rem;
+            font-weight: 500;
+            color: ${Colors.primary};
+        }
+    }
+`
+
+const StyledButtons = styled.div`
+    display: flex;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    margin-top: 3rem;
+
+    button, a {
+        width: 100%;
+        padding: 1.5rem;
+        background: ${Colors.ternary};
+        border-radius: 10px;
+        box-shadow: ${Shadow.altSmall};
+        transition: ${Transition.normal};
+        font-size: 2rem;
+        font-weight: 700;
+        text-align: center;
+        
+        &:hover {
+            transform: translateY(-5px);
+            background: ${Colors.primary};
+            color: ${Colors.secondary};
+        }
+        
+    }
+    
+    button {
+        color: ${Colors.secondary};
+        margin-left: 1.5rem;
+        background: ${Colors.primary};
+        
+        &:hover {
+            background: ${Colors.ternary};
+            color: ${Colors.primary};
+        }
+    }
+`
+
 export const Login = ({ setToken, setFamilyMemberId }:LoginProps) => {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword] = useState('');
@@ -68,22 +170,31 @@ export const Login = ({ setToken, setFamilyMemberId }:LoginProps) => {
     }
     
     return (
-        <div className="login-wrapper">
-            <h1>Please Log In</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <input type="text" onChange={e => setUsername(e.target.value)}/>
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" onChange={ e=> setPassword(e.target.value) } />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
+        <StyledDiv>
+
+            <StyledLogo>
+                <img src={logo} alt="Logo of Life log" />
+            </StyledLogo>
+
+            <StyledContainer>
+                <h2>Please sign in</h2>
+
+                <StyledForm onSubmit={handleSubmit}>
+                    <label>
+                        Username
+                        <input required type="text" onChange={e => setUsername(e.target.value)}/>
+                    </label>
+                    <label>
+                        Password
+                        <input required type="password" onChange={ e=> setPassword(e.target.value) } />
+                    </label>
+                    <StyledButtons>
+                        <a href="/">Register</a>
+                        <button type="submit">Sign in</button>
+                    </StyledButtons>
+                </StyledForm>
+            </StyledContainer>
+        </StyledDiv>
     )
 }
 
