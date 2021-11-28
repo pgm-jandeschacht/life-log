ca97230052eaee9401f4b1fdcc3604cf1aaefdf3
pgm-jandeschacht <jandesch@student.arteveldehs.be>
2021-11-28 18:06:03 +0100

    Made agenda responsive

--- client/src/components/agenda/AgendaList.tsx
+++ client/src/components/agenda/AgendaList.tsx
@@ -8,6 +8,7 @@ import { GET_AGENDAITEMS_BY_FAMILYMEMBER_ID} from '../../graphql/agendaItems';
 import { FamilyMemberData, AgendaItemsData } from '../../interfaces';
 import _ from 'lodash';
 import { Loading } from '../alerts';
+import { Breakpoint } from '../../variables';
 
 
 // example object agenda
@@ -33,15 +34,33 @@ const agenda = [
                 "I will have lasagna for dinner at 6:00 PM",
             ]
         } 
-    }
+    },
+    {
+        id: 3,
+        day: {
+            date: "16 November, 2021",
+            content: [
+                "I will have bread with jam for lunch at 12:00 PM",
+                "I will play bingo in de common room at 3:00 PM",
+                "Karina Cox will visit you at 5:30 PM for her weekly visit",
+                "I will have lasagna for dinner at 6:00 PM",
+            ]
+        } 
+    },
 ]
 
 const StyledUl = styled.ul`
     display: flex;
     flex-direction: column;
+    @media (min-width: ${Breakpoint.large}) {
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        justify-content: flex-start;
+    }
 `
 
-const AgendaList = () => {
+const AgendaList: React.FC = () => {
     // agenda.map(agendaItem => console.log(agendaItem.id))
     
     const familyMemberId = localStorage.getItem('familyMemberId') || '';
@@ -62,12 +81,12 @@ const AgendaList = () => {
     // console.log(sortedAgendaItems['2021-10-27T18:48:22.281Z'])
     return (
         <StyledUl>
-            {  reverseAgenda.map(agendaItem => (
+            {/* {  reverseAgenda.map(agendaItem => (
                 <DayList test={agendaItem} keyId={agendaItem.id} />
-            )) }
-            {/* { agenda.map(agendaItem => (
-                <DayList test={agendaItem.day} keyId={agendaItem.id} />
             )) } */}
+            { agenda.map(agendaItem => (
+                <DayList test={agendaItem.day} keyId={agendaItem.id} />
+            )) }
         </StyledUl>
     )
 }
